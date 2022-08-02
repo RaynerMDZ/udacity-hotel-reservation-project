1d1479693c0a1adf58579f74569a114eb6dca7f1
raynermdz <raynermendezg@gmail.com>
2022-08-02 03:46:18 -0400

    Add api boilerplate code.

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -1,2 +1,41 @@
-package api;public class AdminResource {
+package api;
+
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+import service.CustomerService;
+import service.CustomerServiceImpl;
+import service.ReservationService;
+import service.ReservationServiceImpl;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class AdminResource {
+    public static Long adminCount = 0L;
+
+    private final CustomerService customerService = CustomerServiceImpl.getInstance();
+    private final ReservationService reservationService = ReservationServiceImpl.getInstance();
+
+    public Customer getCustomer(String email) {
+        return null;
+    }
+
+    public void addRoom(List<IRoom> rooms) {
+        adminCount++;
+    }
+
+    public Collection<IRoom> getAllRooms() {
+        return null;
+    }
+
+    public Collection<Customer> getAllCustomers() {
+        return null;
+    }
+
+    public void displayAllReservations() {
+
+    }
+
 }
--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,2 +1,41 @@
-package api;public class HotelResource {
+package api;
+
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+import service.CustomerService;
+import service.CustomerServiceImpl;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class HotelResource {
+    public static Long hotelCount = 0L;
+
+    private final CustomerService customerService = CustomerServiceImpl.getInstance();
+
+    public Customer getCustomer(String email) {
+        return null;
+    }
+
+    public void createACustomer(String email, String firstName, String lastName) {
+        hotelCount++;
+    }
+
+    public IRoom getRoom(String roomNumber) {
+        return null;
+    }
+
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
+        return null;
+    }
+
+    public Collection<Reservation> getCustomerReservations(String customerEmail) {
+        return null;
+    }
+
+    public Collection<IRoom> findARoom(Date checkInDate, Date checkOutDate) {
+        return null;
+    }
+
 }
